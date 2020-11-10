0f81be413b99b9ef8be64b75f99b5d517e04dbee
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-10 15:00:55 +0200

    altered styling, added state and basic structure of app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  // state
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('yellow');    
+  }
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery buddy</h3>
+        <div className="form-control">
+          <input 
+            type="text" 
+            className="grocery" 
+            placeholder="e.g. carrots" 
+            value={name}
+            onChange={(e) => setName(e.target.value)} />
+          <button type="submit" className="submit-btn" title={isEditing ? 'Edit' : 'Submit'}>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn" title="Clear items">clear items</button>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
