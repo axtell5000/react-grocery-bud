9bda3f0ad1ff884473eb09009df246231b5a9583
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-11 12:51:57 +0200

    working with notification alerts, clearing the list and removing a specific item

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-const Alert = () => {
-  return <h2>alert component</h2>
-}
+const Alert = ({ type, msg, removeAlert, list }) => {
+  // useeffect
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]); // now we run this everytime the list changes, instead of just once
 
-export default Alert
+
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
--- src/App.js
+++ src/App.js
@@ -8,17 +8,42 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [alert, setAlert] = useState({ show: true, msg: '', type: '' });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('yellow');    
+    if (!name) {
+      // warning alert
+      showAlert(true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      // editing
+    } else {
+      showAlert(true, 'success', 'item added to the list');
+      // change state with new value
+      const newItem = { id: new Date().getTime().toString(), title: name }; // cheating with id
+      setList([...list, newItem]); // ...list is previous state
+      setName('');
+    }
   }
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter(item => item.id !== id));
+  };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery buddy</h3>
         <div className="form-control">
           <input 
@@ -32,10 +57,15 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn" title="Clear items">clear items</button>
-      </div>
+      { 
+        // only show if items in list
+        list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} />
+          <button className="clear-btn" title="Clear items" onClick={clearList}>clear items</button>
+        </div>
+      )}
+    
     </section>
   );
 }
